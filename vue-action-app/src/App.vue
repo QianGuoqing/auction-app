ca2d80ee63684b5b96db736845360189207fb37a
QianGuoqing <qian_guoqing@yeah.net>
2017-12-26 12:42:14 +0800

    在App.vue设置整体布局，使用bootstrap，布局各个组件

--- vue-action-app/src/App.vue
+++ vue-action-app/src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav-bar></nav-bar>
+    <div class="container">
+      <div class="col-md-3">
+        <search></search>
+      </div>
+      <div class="col-md-9">
+        <div class="row">
+          <carousel></carousel>
+        </div>
+        <div class="row">
+          <product></product>
+        </div>
+      </div>
+    </div>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import NavBar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+import Search from './components/search/search'
+import Carousel from './components/carousel/carousel'
+import Product from './components/product/product'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    NavBar,
+    Search,
+    Carousel,
+    Product,
+    'app-footer': Footer
+  }
 }
 </script>
 
