d1792cd5b0fbcf00ae387793d0a6c355eeb7a627
QianGuoqing <qian_guoqing@yeah.net>
2017-12-27 13:52:34 +0800

    添加search组件的表单验证功能，使用iview

--- vue-action-app/src/components/search/search.vue
+++ vue-action-app/src/components/search/search.vue
@@ -1,26 +1,59 @@
 <template>
-  <form>
-    <div class="form-group">
-      <label for="productName">商品名称: </label>
-      <input type="text" id="productName" placeholder="商品名称" class="form-control">
-    </div>
-    <div class="form-group">
-      <label for="productPrice">商品价格: </label>
-      <input type="text" id="productPrice" placeholder="商品价格" class="form-control">
-    </div>
-    <div class="form-group">
-      <label for="productCategory">商品分类: </label>
-      <select name="" id="productCategory" class="form-control"></select>
-    </div>
-    <div class="form-group">
-      <button type="button" class="btn btn-primary btn-block">搜索</button>
-    </div>
-  </form>
+  <Form ref="formValidate" :model="formValidate" :rules="ruleValidate">
+    <FormItem label="商品名称" prop="productTitle">
+      <Input v-model="formValidate.productTitle" placeholder="商品名称"></Input>
+    </FormItem>
+    <FormItem label="商品价格" prop="productPrice">
+      <Input v-model="formValidate.productPrice" placeholder="商品价格"></Input>
+    </FormItem>
+    <FormItem label="商品类别" prop="productCategory">
+      <Select placeholder="选择商品" v-model="formValidate.prodcutCategory">
+        <Option v-for="(category, index) in product.categories" :key="index" :value="category">{{ category }}</Option>
+      </Select>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" long>搜索</Button>
+    </FormItem>
+  </Form>
 </template>
 
 <script>
   export default {
-    
+    data () {
+      return {
+        product: {
+          categories: ['电子产品', '教育', '图书']
+        },
+        formValidate: {
+          productTitle: '',
+          productPrice: '',
+          productCategory: []
+        },
+        ruleValidate: {
+          productTitle: [
+            {
+              required: true,
+              message: 'The product title cannot be empty',
+              trigger: 'blur'
+            }
+          ],
+          productPrice: [
+            {
+              required: true,
+              message: 'The product price cannot be empty',
+              trigger: 'blur'
+            }
+          ],
+          productCategory: [
+            {
+              required: true,
+              message: 'The product category cannot be empty',
+              trigger: 'blur'
+            }
+          ]
+        }
+      }
+    }
   }
 </script>
 
